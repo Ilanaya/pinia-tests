afeb2e914dd43fd3717317e7b790058e14856e78
Ilya Golovin <me@ilscc.ru>
2023-04-13 01:23:54 +0300

    simple solution

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import { getActivePinia } from "pinia";
 import TheWelcome from "../components/TheWelcome.vue";
+import { useCounterStore } from "../stores/counter";
+
+const counterStore = useCounterStore();
+counterStore.count++;
+
+const getStoreById = (id: string) => {
+  //@ts-expect-error _s is internal
+  return getActivePinia()?._s.get(id);
+};
+const foundStore = getStoreById("counter");
+console.log("foundStore", foundStore);
 </script>
 
 <template>
